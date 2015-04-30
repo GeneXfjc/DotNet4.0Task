74bb32a7971ce274eec6e499abf874e2a179c93a
GeneXfjc <tiki02@fujic.info>
2015-04-30 18:45:37 +0900

    new file:   Base40UseAsync/Base40UseAsync.sln 	new file:   Base40UseAsync/Base40UseAsync.userprefs 	new file:   Base40UseAsync/Base40UseAsync/Base40UseAsync.csproj 	new file:   Base40UseAsync/Base40UseAsync/Program.cs 	new file:   Base40UseAsync/Base40UseAsync/Properties/AssemblyInfo.cs 	new file:   Base40UseAsync/Base40UseAsync/UseClass.cs 	new file:   Base40UseAsync/Base40UseAsync/bin/Debug/Base40UseAsync.exe 	new file:   Base40UseAsync/Base40UseAsync/bin/Debug/Base40UseAsync.exe.mdb 	new file:   Base40UseAsync/Base40UseAsync/obj/x86/Debug/.NETFramework,Version=v4.5.AssemblyAttribute.cs 	new file:   Base40UseAsync/Base40UseAsync/obj/x86/Debug/Base40UseAsync.csproj.FilesWrittenAbsolute.txt 	new file:   Base40UseAsync/Base40UseAsync/obj/x86/Debug/Base40UseAsync.exe 	new file:   Base40UseAsync/Base40UseAsync/obj/x86/Debug/Base40UseAsync.exe.mdb

---
+++ Base40UseAsync/Base40UseAsync/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Base40UseAsync
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var IDs = Enumerable.Range(11, 10);
+            var used = new UseClass();
+            //Async
+            var tasks = IDs.Select(used.AsyncMethod);
+            var taskEnume = new AsyncEnumerable<string>(tasks);
+            //Sync
+            foreach (var item in IDs.Select(i=>used.AsyncMethod(i)))
+            {
+                    Console.WriteLine (item.Result);
+            }
+//            for (int i = 0; i < 20; i++)
+//            {
+//                System.Threading.Thread.Sleep(100);
+//            }
+            StopWatchWrite(sw, "SyncMethod");
+
+            //Async
+            foreach (var item in taskEnume)
+            {
+                Console.WriteLine(item);
+            }
+            StopWatchWrite(sw, "EndAsync");
+            var taks03 = IDs.Select(i =>
+            {
+                    var resTask =used.AsyncMethod(i);
+                    //なんらかの加工が必要なばい
+                    //task処理後のTaskをContinueWith内で処理する。
+                    //終了タスクに変更を加える
+                    return  resTask.ContinueWith(
+                                tRes=> new NameInfo(i,tRes)
+                            );
+            });
+            var Async03 = new AsyncEnumerable<NameInfo>(taks03);
+            foreach (var item in Async03)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void StopWatchWrite(Stopwatch sw,string msg)
+        {
+            Console.WriteLine("{0}:{1}",msg,sw.ElapsedMilliseconds);
+        }
+
+    }
+}
---
+++ Base40UseAsync/Base40UseAsync/UseClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Base40UseAsync
+{
+    //LinqTask<T>から結果を非同期で列挙します。
+    public class AsyncEnumerable<T>:IEnumerable<T>
+    {
+        List<Task<T>> _Tasks;
+        public AsyncEnumerable(IEnumerable<Task<T>> task)
+        {
+            _Tasks = task.ToList();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            //必要に応じてキャッシュを利用することも考えるべき
+            //特にキャンセルがこの処理から利用できると嬉しいかも
+            var tasks = new List<Task<T>>(_Tasks);
+            while (tasks.Count > 0)
+            {
+                var endTask = Task.WhenAny(tasks);
+                var t = endTask.Result;
+                tasks.Remove(t);
+                yield return t.Result;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() 
+        {
+            return this.GetEnumerator(); 
+        } 
+    }
+    public class NameInfo
+    {
+        public NameInfo(int id,string name)
+        {
+            Index = id;
+            _Name = name;
+        }
+        public NameInfo(int id,Task<string> tname)
+        {
+            Index = id;
+            tName = tname;
+        }
+        public int Index { get; set; }
+        private string _Name;
+        public string Name 
+        {
+            get{ return _Name ?? tName.Result;}
+            set{ _Name = value;} 
+        }
+        public Task<string> tName { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Inde:{0:d3},Name: {1}",Index,Name);
+        }
+    }
+    public class UseClass
+    {
+        public UseClass()
+        {
+        }
+        public string SyncMethod(int i)
+        {
+            return string.Format("Name{0:D3}", i);
+        }
+        public Task<string> AsyncMethod(int i)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                System.Threading.Thread.Sleep(800);
+                return SyncMethod(i);
+            });
+        }
+    }
+}
+
